02f8c61b31b8a5d88df022b798d39bd8cd6933c4
chenmic8 <chenmic8@msu.edu>
2023-04-23 13:05:28 -0400

    routing of projects table, tasks table, and implement task modals

---
+++ routes/projects.js
@@ -0,0 +1,98 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  //get list of projects
+  const projects = [
+    {
+      id: 01,
+      title: "project 1",
+      state: "new",
+      order: 1,
+      description: "new test project 1",
+      dateCreated: "01/01/1999",
+    },
+    {
+      id: 02,
+      title: "project 2",
+      state: "new",
+      order: 2,
+      description: "new test project 2",
+      dateCreated: "02/02/1999",
+    },
+    {
+      id: 03,
+      title: "project 3",
+      state: "new",
+      order: 3,
+      description: "new test project 3",
+      dateCreated: "03/03/1999",
+    },
+    {
+      id: 04,
+      title: "project 4",
+      state: "new",
+      order: 4,
+      description: "new test project 4",
+      dateCreated: "04/04/1999",
+    },
+  ];
+
+  res.render("projects", { projects });
+});
+router.get("/:projectTitle", function (req, res, next) {
+  //get list of projects
+  const tasks = [
+    {
+      id: 01,
+      title: "task 1",
+      order: 1,
+      description: "new test task 1",
+      dateCreated: "01/01/1999",
+      estimatedTime: 1,
+      percentComplete: 0,
+      state: "new",
+      project: req.params.projectTitle,
+    },
+    {
+      id: 02,
+      title: "task 2",
+      order: 2,
+      description: "new test task 2",
+      dateCreated: "02/02/1999",
+      estimatedTime: 2,
+      percentComplete: 0,
+      state: "new",
+      project: req.params.projectTitle,
+    },
+    {
+      id: 03,
+      title: "task 3",
+      order: 3,
+      description: "new test task 3",
+      dateCreated: "03/03/1999",
+      estimatedTime: 3,
+      percentComplete: 0,
+      state: "new",
+      project: req.params.projectTitle,
+    },
+    {
+      id: 04,
+      title: "task 4",
+      order: 4,
+      description: "new test task 4",
+      dateCreated: "04/04/1999",
+      estimatedTime: 4,
+      percentComplete: 0,
+      state: "new",
+      project: req.params.projectTitle,
+    },
+  ];
+  console.log("project title: ", req.params.projectTitle);
+  res.render("tasks", {
+    data: { tasks: tasks, project: req.params.projectTitle },
+  });
+});
+
+module.exports = router;
